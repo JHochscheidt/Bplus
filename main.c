cfb66bc11746c9e265eff57af8837f9252a1eb09
trichez <trichez>
2015-11-29 00:14:11 -0200

    pronta? //revisar e apagar comentarios

--- main.c
+++ main.c
@@ -2,15 +2,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define M atoi(argv[1])// ordem da arvore
 
-
+/*
+ * ./a.out
+ *  numero_do_atributo 
+ * 	numero_caracteres_indexados
+ *  ordem_arvore
+ *	
+ * */
 int main(int argc, char **argv){
 	
-	printf("M [%d]\n", M);
+	//printf("M [%d]\n", M);
 	
+	int arg1 = atoi(argv[1]);// numero_do_atributo
+	int arg2 = atoi(argv[2]);// numero_caracteres_indexados
+	int M = atoi(argv[3]);// ordem da arvore
 	//int a;
 	TpTree *arv = novaArv();
+	char *buffer = malloc(sizeof(char)*512);
+	char *pch = NULL;
+	char *reg =NULL;
+	FILE *pFile = fopen("arquivo_teste.csv", "r");
+	int campo = 1;
+	
+	fgets(buffer, 150, pFile); // pula a primeira linha onde tem o nome dos atributos
+	while(fgets(buffer, 511, pFile)!= NULL){
+
+		pch = strtok (buffer, ",\"");//vai até a info do primeiro campo
+		if(pch[0] == '\n' || pch[0] == EOF || pch == NULL){
+			break;
+		}
+		else{
+			campo = 1;
+			do{
+				if(campo == arg1){// copiar os n(param) caracter aq strncpy() 
+					if(arg2 < (int)(strlen(pch)-1)){
+						reg = malloc(sizeof(char)*(arg2+1));
+						strncpy(reg, pch, arg2);
+						insereWord(arv, arv->raiz, pch, M);
+					}else//pch é menor doq o tamanho de caracterres à serem indexados
+						insereWord(arv, arv->raiz, pch, M);
+				}
+				if(pch[0] == ' '){
+					
+					pch = strtok (buffer,  "\"");
+					while(pch[0]==','){
+						campo++; //
+						pch = strtok (NULL,  "\"");						
+					}
+				}
+				while(!isalnum(pch[0]) &&  pch[0] != '\n');{			
+					pch = strtok (NULL,  "\"");//vai p/ prox iterração apenas qnd 
+					if(pch != NULL){
+						while(pch[0]==','){							
+							campo++; //
+							pch = strtok (NULL,  "\"");
+						}	
+					}
+				}
+				if(pch == NULL)
+					break;	
+			}while(pch[0] != '\n' || pch[0] == EOF || pch == NULL);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/*insereWord(arv, arv->raiz, "ceéobichao", M);
 	insereWord(arv, arv->raiz, "jackson", M);
@@ -56,7 +124,7 @@ int main(int argc, char **argv){
 	insereWord(arv, arv->raiz, "ej", M);
 	insereWord(arv, arv->raiz, "easease", M);
 	*/
-	
+	/*
 	insereWord(arv, arv->raiz, "a", M);
 	insereWord(arv, arv->raiz, "a", M);
 	insereWord(arv, arv->raiz, "a", M);
@@ -154,7 +222,7 @@ int main(int argc, char **argv){
 	insereWord(arv, arv->raiz, "y", M);
 	insereWord(arv, arv->raiz, "z", M);
 	insereWord(arv, arv->raiz, "z", M);
-	
+	*/	
 	
 	imprimePaginas(arv, arv->folhaE);
 	//printf("\nRAIZ[%s] end[%p]\n", arv->raiz->palavras->first->word, arv->raiz);
